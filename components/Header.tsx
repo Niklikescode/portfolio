4cdba60d17bd1a9a94f18e82cc79472b1d2a5975
Niklikescode <niklikescode@gmail.com>
2024-02-06 17:37:20 +0000

    header responsive buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import { SocialIcon } from 'react-social-icons'
+import React from "react";
+import { SocialIcon } from "react-social-icons";
 
-
-type Props = {}
+type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header>
+    <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center"> 
       <div>
-      <SocialIcon url="https://twitter.com" />
+        <SocialIcon
+          url="https://youtube.com"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://youtube.com"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://youtube.com"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+      </div>
+
+      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in Touch!</p>
       </div>
     </header>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home() {
 
 
 
-      <h1 className="text-blue-800">Let's build a portfolio</h1>
+     
     </div>
   )
 }
