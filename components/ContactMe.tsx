52afb0519d91129f4831670064a6c7aa148bcf13
Niklikescode <niklikescode@gmail.com>
2024-02-19 16:31:48 +0000

    contact info set up

---
+++ components/ContactMe.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+
+type Props = {};
+
+function ContactMe({}: Props) {
+  return (
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Contact
+      </h3>
+
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">
+          I'd love to hear from you. <br />
+          <span className="underline decoration-orange-400/50">Lets Talk!</span></h4>
+
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p>+447931147153</p>
+          </div>
+
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p>niklikescode@gmail.com</p>
+          </div>
+
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p>Berkshire, United Kingdom</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactMe;
