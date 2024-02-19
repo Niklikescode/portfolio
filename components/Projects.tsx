fdd4b4688959fe8686c8849f2dd0275277c4c952
Niklikescode <niklikescode@gmail.com>
2024-02-19 15:37:30 +0000

    projects layout with transitions complete

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,11 +1,17 @@
+import { motion } from "framer-motion";
 import React from "react";
+
 
 type Props = {};
 
 function Projects({}: Props) {
   const projects = [1, 2, 3, 4, 5];
   return (
-    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+    <motion.div
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    transition={{ duration: 1.5 }} 
+    className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
@@ -13,7 +19,14 @@ function Projects({}: Props) {
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
         {projects.map((project, i) => (
           <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-            <img
+            <motion.img
+            initial={{
+              y: -300,
+              opacity: 0,
+            }}
+            transition={{duration: 1.2}}
+            whileInView={{opacity: 1, y: 0}}
+            viewport={{once:true}}
               src="https://cdn.iconscout.com/icon/premium/png-256-thumb/project-management-536789.png"
               alt=""
             />
@@ -30,7 +43,7 @@ function Projects({}: Props) {
       </div>
 
       <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12" />
-    </div>
+    </motion.div>
   );
 }
 
