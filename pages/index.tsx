6160a7ce74b7a341a90a7315885d35c80d23f56d
Niklikescode <niklikescode@gmail.com>
2024-02-19 14:03:15 +0000

    projects layout complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,7 @@ export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 ">
       <Head>
-        <title>Nikhil's Portfolio</title>
+        <title>Nikhils Portfolio</title>
       </Head>
 
       <Header />
@@ -34,7 +35,9 @@ export default function Home() {
         <Skills />
       </section>
 
-      {/* projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* contact me */}
     </div>
