6df052b6e058716aaa9bd2046716d43e1d8e4f95
Niklikescode <niklikescode@gmail.com>
2024-02-21 14:05:50 +0000

    page info and experience schema made

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion"
-import Link from "next/link";
 
 
 type Props = {};
@@ -42,8 +41,6 @@ export default function Header({}: Props) {
         />
      </motion.div>
 
-     <Link href="#contact">
-
       <motion.div
       initial={{
         x: 500,
@@ -69,7 +66,6 @@ export default function Header({}: Props) {
         />
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in Touch!</p>
       </motion.div>
-      </Link>
     </header>
   );
 }
