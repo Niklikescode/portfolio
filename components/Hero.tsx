01262c48f6478c77ce8237002841e7d90f639cc3
Niklikescode <niklikescode@gmail.com>
2024-03-11 22:05:49 +0000

    increased small screen text size on hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,7 +34,7 @@ function Hero({}: Props) {
       />
       <div className="z-20">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Developer</h2>
-        <h1 className="text-2xl lg:text-6xl font-semibold px-10">
+        <h1 className="text-3xl lg:text-6xl font-semibold px-10">
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="purple" />
         </h1>
