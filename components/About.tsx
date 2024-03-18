e61cb59d53d633893cbccbd8212352c4c64c6783
Niklikescode <niklikescode@gmail.com>
2024-03-18 20:19:17 +0000

    resizing

--- components/About.tsx
+++ components/About.tsx
@@ -15,7 +15,7 @@ function About({}: Props) {
       transition={{ duration: 1.5 }}
       className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-16 uppercase tracking-[20px] text-gray-500 text-1xl md:text-2xl ml-4">
+      <h3 className="absolute top-20 uppercase tracking-[20px] text-gray-500 text-1xl md:text-2xl ml-4">
         About
       </h3>
 
@@ -41,14 +41,14 @@ function About({}: Props) {
       </motion.div>
 
       <div className="space-y-10 px-0 md:px-10">
-        <h4 className="-mb-4 mt-4 text-2xl lg:text-4xl font-semibold ">
+        <h4 className="-mb-4 mt-4 text-[22px] lg:text-4xl font-semibold ">
           Here&apos;s a{" "}
           <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
           background about me...<span></span>
         </h4>
 
-        <div className="overflow-y-scroll scrollbar-thin h-72 scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 ">
-          <p className="text-left flex-auto">
+        <div className=" overflow-y-scroll scrollbar-thin h-72 scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 ">
+          <p className="text-left">
             Firstly, thanks for stopping by! I guess a good place to begin would
             be to explain why I&apos;m here. Let&apos;s get the boring bit out
             the way... my professional career up to now has predominantly been
